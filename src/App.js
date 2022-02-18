c81609a1429ef2d080d802a2b69a148d011557ed
Hatice Koçak <30705864+kocakkhatice@users.noreply.github.com>
2022-02-18 13:38:10 +0300

    fix conflict

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import "./App.css";
 import Residences from "./components/Residences";
+import Private_Office from "./components/Private_Office";
+
 import Form from "./components/Form";
 
 function App() {
   return (
     <div>
       <Residences />
+      <Private_Office />
       <Form />
     </div>
   );
