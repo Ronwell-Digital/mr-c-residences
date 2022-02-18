c9920aaa05018f7b38668b844a8d44e3b1eb26be
Barış Can Çifçi <78167969+Bariscncfc@users.noreply.github.com>
2022-02-18 13:06:17 +0300

    fixed

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
 
 import './App.css';
 import Residences from "./components/Residences";
-import Private_Office from "./components/Private_Office";
+
 
 function App() {
   return (
     <div>
      <Residences/>
-        <Private_Office/>
+
     </div>
   );
 }
