c25dad71796dea5301d547d1872f26417642a933
Hatice Koçak <30705864+kocakkhatice@users.noreply.github.com>
2022-02-25 11:40:14 +0300

    reset project

--- src/App.js
+++ src/App.js
@@ -1,19 +1,7 @@
 import "./App.css";
-import Residences from "./components/Residences";
-import Private_Office from "./components/Private_Office";
-
-import Form from "./components/Form";
-import Footer from "./components/Footer";
 
 function App() {
-  return (
-    <div>
-      <Residences />
-      <Private_Office />
-      <Form />
-        <Footer/>
-    </div>
-  );
+  return <div>hello world</div>;
 }
 
 export default App;
