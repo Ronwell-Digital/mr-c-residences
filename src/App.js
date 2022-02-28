c6bfdb1c1b12fb3944503bbf7390bcf42b12d1e0
Barış Can Çifçi <78167969+Bariscncfc@users.noreply.github.com>
2022-02-28 16:48:44 +0300

    footer component is created

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import PaymentTerms from "./Components/Payment Terms/PaymentTerms";
 import Inspiration from "./Components/Inspiration/Inspiration";
 import Gallery from "./Components/Gallery/Gallery";
 import Contact from "./Components/Contact/Contact";
+import Footer from "./Components/Footer/Footer";
 
 function App() {
   return <div>
@@ -10,6 +11,7 @@ function App() {
     <Inspiration/>
     <Gallery/>
     <Contact/>
+    <Footer/>
   </div>;
 }
 
